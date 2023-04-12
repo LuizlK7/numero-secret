d359d8e681f29ed40dc4abc59975b12b7672abf2
LuizlK7 <109628899+LuizlK7@users.noreply.github.com>
2023-04-12 09:50:09 -0300

    Add files via upload

---
+++ numero-secreto/app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+    // + 1 porque o Math.random vai até um valor menor do número referenciado
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log('Número secreto: ' + numeroSecreto)
+
+const elementoMenorValor = document.querySelector('#menor-valor')
+const elementoMaiorValor = document.querySelector('#maior-valor')
+
+elementoMenorValor.textContent = menorValor
+elementoMaiorValor.textContent = maiorValor
